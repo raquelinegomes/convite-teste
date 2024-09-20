e2d1edfdb5b997cad2d35b7713da4eabbee7e323
raquelinegomes <raquelygomes08@gmail.com>
2024-09-20 17:39:43 -0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 document.getElementById("yesBtn").addEventListener("click", function() {
     const message = document.getElementById("responseMessage");
-    message.innerHTML = "Hmmm, certo, legal , otima decisão, inteligente, te espero a partir das 19h!";
+    message.innerHTML = "Hmm, certo, legal, ótima decisão, inteligente, te espero a partir das 19h!";
     const card = document.getElementById("responseCard");
     card.className = "response-card valid";
     card.classList.remove("hidden"); 
